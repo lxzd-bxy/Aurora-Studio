b7f723f78649111e1f1f2a116b99e3db781a167d
lxzd-bxy <lxzdbxy@proton.me>
2025-05-11 22:12:19 +0500

    first commit

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <gtk/gtk.h>
+
+static void on_button_clicked(GtkWidget *widget, gpointer data) {
+    g_print("Button clicked!\n");
+}
+
+int main(int argc, char *argv[]) {
+    GtkWidget *window;
+    GtkWidget *button;
+    
+    gtk_init(&argc, &argv);
+    
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    
+    button = gtk_button_new_with_label("Click me!");
+    g_signal_connect(button, "clicked", G_CALLBACK(on_button_clicked), NULL);
+    
+    gtk_container_add(GTK_CONTAINER(window), button);
+    gtk_widget_show_all(window);
+    
+    gtk_main();
+    
+    return 0;
+}
